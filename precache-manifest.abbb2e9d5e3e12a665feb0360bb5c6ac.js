d4acb4791db893ceb6b7f6593eba87d5116dfb0a
kushaal29 <104088156+kushaal29@users.noreply.github.com>
2023-11-16 14:38:01 +0530

    Updates

---
+++ precache-manifest.abbb2e9d5e3e12a665feb0360bb5c6ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78e0f8951dc326845383c2209ca564f",
+    "url": "/RJsCourse/index.html"
+  },
+  {
+    "revision": "e17b5e2be3af0226d690",
+    "url": "/RJsCourse/static/css/main.a81f5e1c.chunk.css"
+  },
+  {
+    "revision": "8946a0edaa5c8cb07476",
+    "url": "/RJsCourse/static/js/2.2a068b1b.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/RJsCourse/static/js/2.2a068b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c22086003eed385c50",
+    "url": "/RJsCourse/static/js/3.b5bd36bd.chunk.js"
+  },
+  {
+    "revision": "e17b5e2be3af0226d690",
+    "url": "/RJsCourse/static/js/main.473ae1b6.chunk.js"
+  },
+  {
+    "revision": "1540023a9d1be9c5dc8b",
+    "url": "/RJsCourse/static/js/runtime-main.bc2ce59c.js"
+  }
+]);
